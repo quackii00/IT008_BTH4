13f3e84c0883357ff9fd0f5ec88c63e7c0995c8e
Nguyen Thi Ngoc Huyen <hinduck3206@gmail.com>
2025-11-29 08:19:43 +0700

    Hoàn thành bài 1

---
+++ Bai01/Bai01/MainForm.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace Bai01
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 722);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mouse and Key";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Key_Down);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Mouse_Click);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Bai01/Bai01/MainForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai01
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Mouse_Click(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                MessageBox.Show($"Chuột trái được nhấn. Tọa độ ({e.X},{e.Y})", "Thông báo");
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                MessageBox.Show($"Chuột phải được nhấn. Tọa độ ({e.X},{e.Y})","Thông báo");
+            }
+            else if (e.Button == MouseButtons.Middle)
+            {
+                MessageBox.Show($"Chuột giữa được nhấn. Tọa độ ({e.X},{e.Y})","Thông báo");
+            }
+        }
+
+        private void Key_Down(object sender, KeyEventArgs e)
+        {
+            int keyCode = e.KeyValue;
+            string keyName = e.KeyCode.ToString();
+            char asciiChar = (char)keyCode;
+            string keyData= e.KeyData.ToString();
+            MessageBox.Show($"Phím được nhấn:\nCó mã: {keyName}\nKeyCode: {keyCode}\nASCII: {(int)asciiChar}\nKeydata:{keyData}","Thông báo");
+        }
+    }
+}
